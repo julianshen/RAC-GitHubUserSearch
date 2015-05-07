94245a6a0ae890f68d3121f8b37dd5fde8501a4a
Julian Shen <julianshen@gmail.com>
2015-05-07 19:06:49 +0800

    add gitignore

---
+++ GithubUserSearch/UserCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserCell.swift
+//  GithubUserSearch
+//
+//  Created by Julian Shen on 2015/5/7.
+//  Copyright (c) 2015年 Julian Shen. All rights reserved.
+//
+
+import UIKit
+
+class UserCell:UITableViewCell {
+    @IBOutlet weak var avatarImageView:UIImageView!
+    @IBOutlet weak var userNameLabel:UILabel!
+}
